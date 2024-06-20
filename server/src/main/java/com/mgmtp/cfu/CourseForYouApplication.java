935770f084ee2b0acd525c083749650b7681f5a4
NDKhoa1307 <khoa130704@gmail.com>
2024-06-20 16:35:02 +0700

    Initial server commit
    Jira: MGMIDN-1115

---
+++ server/src/main/java/com/mgmtp/cfu/CourseForYouApplication.java
@@ -0,0 +1,11 @@
+package com.mgmtp.cfu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseForYouApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(CourseForYouApplication.class, args);
+	}
+}
