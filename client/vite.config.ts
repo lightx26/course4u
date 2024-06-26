d37084d16e42c0671799e70794019a1aea4dfc3f
vulinhnopro2704 <vulinhtruong79@gmail.com>
2024-06-26 09:19:57 +0700

    Update docker file

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,10 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    port: 8080,
+    strictPort: true,
+  },
   server: {
-    host: '0.0.0.0',
-  }
-})
+    port: 8080,
+    strictPort: true,
+    host: true,
+    origin: "http://0.0.0.0:8080",
+  },
+});
