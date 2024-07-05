ab1e56d15ae9c74f911250261a19ee7a49bfc806
NDKhoa1307 <khoa130704@gmail.com>
2024-07-05 09:52:12 +0700

    Feature: Fix client warning/errors
    Fixed warning on client console
    Fixed image errors

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,16 +1,23 @@
-import { defineConfig } from "vite";
-import viteReact from "@vitejs/plugin-react";
-// import react from "@vitejs/plugin-react";
+import {defineConfig, loadEnv} from "vite";
+import react from "@vitejs/plugin-react";
+import {UserConfigExport, ConfigEnv} from "vite";
 
-export default defineConfig({
-  plugins: [viteReact()],
-  preview: {
-    port: 5173,
-    strictPort: true,
-  },
-  server: {
-    port: 5173,
-    strictPort: true,
-    host: true,
-  },
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-expect-error
+export default defineConfig(({mode}: ConfigEnv): UserConfigExport => {
+  const env = loadEnv(mode, process.cwd());
+
+  return{
+    plugins: [react()],
+    preview: {
+      port: 5173,
+      strictPort: true,
+    },
+    server: {
+      port: 5173,
+      strictPort: true,
+      host: true,
+    },
+    base: env.VITE_BASE_URL,
+  }
 });
