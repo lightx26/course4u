f3e235212505c0362f383108b939bee8919bb949
= <khoa130704@gmail.com>
2024-07-11 09:56:21 +0700

    [MGMIDN-1229] Set up APIs for admin page
    Set up APIs for Admin to query all registration or some registration based on status (out of scope)

--- server/src/main/java/com/mgmtp/cfu/controller/RegistrationController.java
+++ server/src/main/java/com/mgmtp/cfu/controller/RegistrationController.java
@@ -3,12 +3,14 @@ package com.mgmtp.cfu.controller;
 import com.mgmtp.cfu.dto.registrationdto.RegistrationDetailDTO;
 import com.mgmtp.cfu.service.RegistrationService;
 import lombok.RequiredArgsConstructor;
+import org.apache.coyote.Response;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
@@ -19,7 +21,22 @@ public class RegistrationController {
 
     @GetMapping("/{id}")
     public ResponseEntity<RegistrationDetailDTO> getDetailRegistration(@PathVariable Long id) {
-        return ResponseEntity.ok(registrationService.getDetailRegistration(id));
+        return ResponseEntity.ok(registrationService.getDetailRegistration(id))
+    };
+
+    @GetMapping
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    public ResponseEntity<?> getRegistrationForAdmin(
+            @RequestParam(value = "page", defaultValue = "1") int page,
+            @RequestParam(value = "status", defaultValue = "all") String status)
+    {
+        if (page <= 0){
+            return ResponseEntity.badRequest().body("Page number must be greater than 0");
+        }
+
+        return (status.equalsIgnoreCase("all") || status.equalsIgnoreCase("")) ?
+                ResponseEntity.ok(registrationService.getAllRegistrations(page)):
+                ResponseEntity.ok(registrationService.getRegistrationByStatus(page, status));
     }
 
     @GetMapping("/my-registration")
--- server/src/main/java/com/mgmtp/cfu/repository/RegistrationRepository.java
+++ server/src/main/java/com/mgmtp/cfu/repository/RegistrationRepository.java
@@ -1,6 +1,9 @@
 package com.mgmtp.cfu.repository;
 
 import com.mgmtp.cfu.entity.Registration;
+import com.mgmtp.cfu.enums.RegistrationStatus;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import com.mgmtp.cfu.enums.RegistrationStatus;
 import org.springframework.data.domain.Page;
@@ -11,6 +14,11 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 @Repository
 public interface RegistrationRepository extends JpaRepository<Registration, Long> {
+    @Query("SELECT COUNT(r) FROM Registration r WHERE r.course.id = ?1 and (r.status = 'DONE' or r.status = 'VERIFYING' or r.status='VERIFIED' or r.status='CLOSED')")
+    int countLegitRegistrationInCourse(Long courseId);
+
+
+    Page<Registration> findAllByStatus(RegistrationStatus status, Pageable pageable);
     List<Registration> getByUserId(Long userId, Sort sort);
     @Query("SELECT COUNT(r) FROM Registration r WHERE r.course.id = ?1 and (r.status = 'DONE' or r.status = 'VERIFYING' or r.status='VERIFIED' or r.status='CLOSED')")
     int countLegitRegistrationInCourse(Long courseId);
