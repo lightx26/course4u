5bfe0504ed47e5df84fecd02a4842cb87632dc0e
NguyenTrungNghia <nghiatrung2112003@gmail.com>
2024-07-15 13:53:57 +0700

    MGMIDN-1235: Fix code review

--- server/src/main/java/com/mgmtp/cfu/dto/userdto/UserDto.java
+++ server/src/main/java/com/mgmtp/cfu/dto/userdto/UserDto.java
@@ -2,9 +2,6 @@ package com.mgmtp.cfu.dto.userdto;
 
 import com.mgmtp.cfu.enums.Gender;
 import com.mgmtp.cfu.enums.Role;
-import jakarta.persistence.Column;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
 import lombok.*;
 import org.springframework.web.multipart.MultipartFile;
 import java.time.LocalDate;
