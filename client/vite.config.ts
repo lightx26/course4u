14bd15a819397b5627d64c1f5c347b57a6817c0a
duycan17 <duydk.22git@vku.udn.vn>
2024-07-18 14:45:21 +0700

    MGMIDN-1301 resolve conflict

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -19,8 +19,6 @@ export default defineConfig(({mode}: ConfigEnv): UserConfigExport => {
       host: true,
     },
     base: env.VITE_BASE_URL,
-    optimizeDeps: {
-      exclude: ['chunk-JKEHHYL4.js']
-    }
+
   }
 });
