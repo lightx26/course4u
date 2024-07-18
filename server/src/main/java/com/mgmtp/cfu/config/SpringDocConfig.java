b67254d8dc03e3cc19b14e8de8389d03f3edc1b9
NDKhoa1307 <khoa130704@gmail.com>
2024-07-18 19:59:24 +0700

    [MGMIDN-1193] Created swagger to document APIs
    Added openAPI 3 plugin
    Config global authentication
    Excluded global authentication for login

---
+++ server/src/main/java/com/mgmtp/cfu/config/SpringDocConfig.java
@@ -0,0 +1,23 @@
+package com.mgmtp.cfu.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(info = @Info(title = "Course4U", version = "0.1.0"), security = {@SecurityRequirement(name = "bearer-key")})
+
+public class SpringDocConfig {
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .components(new Components()
+                        .addSecuritySchemes("bearer-key",
+                                new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")));
+    }
+}
