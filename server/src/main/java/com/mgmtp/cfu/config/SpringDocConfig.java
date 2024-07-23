8ece5a73889145e94726017821134863deb52959
NDKhoa1307 <khoa130704@gmail.com>
2024-07-23 10:40:25 +0700

    [MGMIDN-1353] Fix APIs error when deploy
    Added a new variable for prefixes based on current environment
    Reconfigure API end points

--- server/src/main/java/com/mgmtp/cfu/config/SpringDocConfig.java
+++ server/src/main/java/com/mgmtp/cfu/config/SpringDocConfig.java
@@ -6,17 +6,28 @@ import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.List;
+
 @Configuration
-@OpenAPIDefinition(info = @Info(title = "Course4U", version = "0.1.0"), security = {@SecurityRequirement(name = "bearer-key")})
+@OpenAPIDefinition(
+        info = @Info(title = "Course4U", version = "0.1.0"),
+        security = {@SecurityRequirement(name = "bearer-key")}
+)
 public class SpringDocConfig {
+    @Value("${course4u.swagger.environment-prefix}")
+    private String environmentPrefix;
+
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
                 .components(new Components()
                         .addSecuritySchemes("bearer-key",
-                                new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")));
+                                new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")))
+                .servers(List.of(new Server().url(environmentPrefix)));
     }
 }
