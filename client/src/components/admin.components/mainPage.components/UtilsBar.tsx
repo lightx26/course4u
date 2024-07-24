720912b7af1cbc06a2397bcfed6f6dd25b5400ad
nguyenthuchoang <nguyenthuchoang17112003@gmail.com>
2024-07-25 00:03:52 +0700

    [MGMIDN-1322]:Change option status for accountant

--- client/src/components/admin.components/mainPage.components/UtilsBar.tsx
+++ client/src/components/admin.components/mainPage.components/UtilsBar.tsx
@@ -1,62 +1,73 @@
-import {useState} from "react";
+import { useState } from "react";
 import SearchGlass from "../../../assets/images/admin.images/SearchGlass.svg";
 import Select from "../Select";
 
 import registrationStatusList from "../../../utils/registrationStatusList";
 import orderByList from "../../../utils/orderByList.ts";
 
-const UtilsBar = () => {
-    const [searchContent, setSearchContent] = useState("");
-    const [sortStatusId, setSortStatusId] = useState("0");
-    const [sortOrder, setSortOrder] = useState("0");
+import { registrationStatusListForAccountant } from "../../../utils/registrationStatusList";
+import { useSelector } from "react-redux";
+import { RootState } from "../../../redux/store/store.ts";
 
-    return (
-        <div className="h-[10%] utils-bar flex gap-7">
-            <div className="search-bar w-[70%]">
-                <span className="text-[11px] font-normal">Search: </span>
-                <div
-                    className="bg-white flex px-2 py-0.5 items-center"
-                    style={{
-                        border: "1px solid rgba(233, 234, 240, 1)",
-                    }}
-                >
-                    <img
-                        className="max-w-[20px] max-h-[20px] fill-border border-solid border-[rgba(233, 234, 240, 1)]"
-                        src={SearchGlass}
-                        alt="search"
-                    />
-                    <input
-                        className="w-full h-[70%] ml-2 py-2 placeholder-opacity-5 focus:outline-none"
-                        placeholder="Search registration by course name or learner"
-                        value={searchContent}
-                        onChange={(e) => {
-                            setSearchContent(e.target.value);
-                        }}
-                    ></input>
-                </div>
-            </div>
-            <div className="status_select w-[15%]">
-                <span className="text-[11px] font-normal">Status: </span>
-                <Select
-                    listOption={registrationStatusList}
-                    value={sortStatusId}
-                    onSortByChange={(e) => {
-                        setSortStatusId(e.target.value);
-                    }}
-                />
-            </div>
-            <div className="sortby_select w-[15%]">
-                <span className="text-[11px] font-normal">Sort by: </span>
-                <Select
-                    listOption={orderByList}
-                    value={sortOrder}
-                    onSortByChange={(e) => {
-                        setSortOrder(e.target.value);
-                    }}
-                />
-            </div>
+const UtilsBar = () => {
+  const userRole = useSelector((state: RootState) => state.user.user.role);
+  const [searchContent, setSearchContent] = useState("");
+  const [sortStatusId, setSortStatusId] = useState(
+    `${userRole === "ACCOUNTANT" ? "7" : "0"}`
+  );
+  const [sortOrder, setSortOrder] = useState("0");
+
+  return (
+    <div className="h-[10%] utils-bar flex gap-7">
+      <div className="search-bar w-[70%]">
+        <span className="text-[11px] font-normal">Search: </span>
+        <div
+          className="bg-white flex px-2 py-0.5 items-center"
+          style={{
+            border: "1px solid rgba(233, 234, 240, 1)",
+          }}
+        >
+          <img
+            className="max-w-[20px] max-h-[20px] fill-border border-solid border-[rgba(233, 234, 240, 1)]"
+            src={SearchGlass}
+            alt="search"
+          />
+          <input
+            className="w-full h-[70%] ml-2 py-2 placeholder-opacity-5 focus:outline-none"
+            placeholder="Search registration by course name or learner"
+            value={searchContent}
+            onChange={(e) => {
+              setSearchContent(e.target.value);
+            }}
+          ></input>
         </div>
-    );
+      </div>
+      <div className="status_select w-[15%]">
+        <span className="text-[11px] font-normal">Status: </span>
+        <Select
+          listOption={
+            userRole === "ACCOUNTANT"
+              ? registrationStatusListForAccountant
+              : registrationStatusList
+          }
+          value={sortStatusId}
+          onSortByChange={(e) => {
+            setSortStatusId(e.target.value);
+          }}
+        />
+      </div>
+      <div className="sortby_select w-[15%]">
+        <span className="text-[11px] font-normal">Sort by: </span>
+        <Select
+          listOption={orderByList}
+          value={sortOrder}
+          onSortByChange={(e) => {
+            setSortOrder(e.target.value);
+          }}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default UtilsBar;
--- client/src/utils/registrationStatusList.ts
+++ client/src/utils/registrationStatusList.ts
@@ -1,44 +1,59 @@
 const registrationStatusList = [
-    {
-        value: "0",
-        content: "All",
-    },
-    {
-        value: "1",
-        content: "Submitted",
-    },
-    {
-        value: "2",
-        content: "Declined",
-    },
-    {
-        value: "3",
-        content: "Approved",
-    },
-    {
-        value: "4",
-        content: "Done",
-    },
-    {
-        value: "5",
-        content: "Verifying",
-    },
-    {
-        value: "6",
-        content: "Declined (Document)",
-    },
-    {
-        value: "7",
-        content: "Verified",
-    },
-    {
-        value: "8",
-        content: "Close",
-    },
-    {
-        value: "9",
-        content: "Discard",
-    },
+  {
+    value: "0",
+    content: "All",
+  },
+  {
+    value: "1",
+    content: "Submitted",
+  },
+  {
+    value: "2",
+    content: "Declined",
+  },
+  {
+    value: "3",
+    content: "Approved",
+  },
+  {
+    value: "4",
+    content: "Done",
+  },
+  {
+    value: "5",
+    content: "Verifying",
+  },
+  {
+    value: "6",
+    content: "Declined (Document)",
+  },
+  {
+    value: "7",
+    content: "Verified",
+  },
+  {
+    value: "8",
+    content: "Close",
+  },
+  {
+    value: "9",
+    content: "Discard",
+  },
+];
+
+export const registrationStatusListForAccountant = [
+  {
+    value: "5",
+    content: "Verifying",
+  },
+  {
+    value: "6",
+    content: "Declined (Document)",
+  },
+  {
+    value: "7",
+    content: "Verified",
+  },
 ];
 
 export default registrationStatusList;
